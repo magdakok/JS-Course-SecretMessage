ba6a4df186bc0267eb5527e89646f6345bca5edc
magdakok <61601293+magdakok@users.noreply.github.com>
2020-05-27 12:36:56 +0100

    create secret message app

---
+++ script.js
@@ -0,0 +1,21 @@
+const { hash } = window.location;
+
+if (hash) {
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#message-show').classList.remove('hide');
+    document.querySelector('h1').innerText = atob(hash.replace('#', ''));
+}
+
+document.querySelector('form').addEventListener('submit', event =>{
+    event.preventDefault();
+
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
+
+    const input = document.querySelector('#message-input');
+    const encrypted = btoa(input.value);
+
+    const linkInput = document.querySelector('#link-input');
+    linkInput.value = `${window.location.origin}#${encrypted}`;
+    linkInput.select();
+})
